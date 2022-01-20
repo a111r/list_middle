ead70fecf0fc9ff9eef092ffb61220013affac10
a111r <92301153+a111r@users.noreply.github.com>
2022-01-20 10:24:25 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -48,12 +48,12 @@ void itc_pos_neg_separator_lst(const vector <int>& lst, vector <int>& lst1, vect
 
 void itc_odd_even_analysis_lst(const vector <int> &lst){
     setlocale(LC_ALL, "RUS");
-    vector <int> ev;
-    vector <int> unev;
-    itc_odd_even_separator_lst(lst, ev, unev);
+    vector <int> a;
+    vector <int> b;
+    itc_odd_even_separator_lst(lst, a, b);
     cout << "Анализ списка:" << endl;
     cout << "Количество четных чисел: " << ev.size() << "," << '\t'	<< '\t' << "Количество нечетных чисел: " << unev.size() << endl;
-    cout << "Максимальная четная цифра: " << itc_max_lst(ev) << "," << '\t'	<< '\t' << "Максимальная нечетная цифр: " << itc_max_lst(unev) <<"," << endl;
-    cout << "Минимальная четная цифра: "<< itc_min_lst(ev) << "," << '\t' << '\t' << "Минимальная нечетная цифра: "<< itc_min_lst(unev) << "," << endl;
-    cout << "Сумма четных чисел: "<< itc_sumlst(ev) << "," << '\t'	<< '\t' << "Сумма нечетных чисел: "<< itc_sumlst(unev) << "," << endl;
+    cout << "Максимальная четная цифра: " << itc_max_lst(a) << "," << '\t'	<< '\t' << "Максимальная нечетная цифр: " << itc_max_lst(b) <<"," << endl;
+    cout << "Минимальная четная цифра: "<< itc_min_lst(a) << "," << '\t' << '\t' << "Минимальная нечетная цифра: "<< itc_min_lst(b) << "," << endl;
+    cout << "Сумма четных чисел: "<< itc_sumlst(a) << "," << '\t'	<< '\t' << "Сумма нечетных чисел: "<< itc_sumlst(b) << "," << endl;
 }
